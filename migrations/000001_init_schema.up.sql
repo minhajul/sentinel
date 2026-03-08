583fe6fbdf89e1f8df36e21f841bbb14a811a69f
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-03-08 22:29:15 +0600

    Updated

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -15,6 +15,6 @@ CREATE TABLE audit_logs
 CREATE INDEX idx_audit_changes ON audit_logs USING GIN (changes);
 
 -- Create initial partitions (e.g., for current month and next)
-CREATE TABLE audit_logs_2026_01 PARTITION OF audit_logs
-FOR VALUES FROM
-('2026-01-01') TO ('2026-02-01');
+-- CREATE TABLE audit_logs_2026_01 PARTITION OF audit_logs
+-- FOR VALUES FROM
+-- ('2026-01-01') TO ('2026-02-01');
